573ae9291148d89699a8735cabf68f824036ab8c
Florian Stuehlinger <fstuehlinger@student.ethz.ch>
2025-07-03 15:58:33 +0200

    Removed unnecessary  includes, commented out init_ref_results, changed float to fType

--- src/cuda/arccos_cuda.cuh
+++ src/cuda/arccos_cuda.cuh
@@ -1,10 +1,6 @@
 #include <cuda_runtime.h>
-#include <iostream>
-#include"cnpy.h"
-#include<cstdlib>
-#include<map>
-#include<string>
-#include<cstring>
+
+#include "cnpy.h"
 
 
 // Define the floating-point type
@@ -16,15 +12,15 @@ __global__ void compute_kernel(fType* d_data, int size);
 // Function to initialize host data from refrence data
 void init_h(fType* h_data, fType* h_result, const fType* x, const fType* res, int i, int chunksize, int bytes);
 
-// Function to initialize result from reference data (arccos values)
-void init_ref_result(float* ref_result, int size);
+// // Function to initialize result from reference data (arccos values)
+// void init_ref_result(float* ref_result, int size);
 
 // Run the arccos computation using multiple CUDA streams
 int run_arccos(int size, int num_streams);
 
 // Run stream operations
-cudaError_t run_stream_operations(float* h_data[], float* h_result[], float* d_data[], cudaStream_t streams[], int size_per_stream, int num_streams,
+cudaError_t run_stream_operations(fType* h_data[], fType* h_result[], fType* d_data[], cudaStream_t streams[], int size_per_stream, int num_streams,
                                      int threads, int blocks);
 
 // Verify the result of the arccos computation (return bool?) (call init_ref_result for the reference result)
-void verify_result(float* h_result[], int size_per_stream, int num_streams);
+void verify_result(fType* h_result[], int size_per_stream, int num_streams);
