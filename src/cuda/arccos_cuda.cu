e1aacf7db41af4a9f1e9db50f617345e1ae75ff4
Raphael Graf <raphael.graf01@gmail.com>
2025-07-08 19:26:11 +0200

    fix again, forgot in if statement to change tol

--- src/cuda/arccos_cuda.cu
+++ src/cuda/arccos_cuda.cu
@@ -159,9 +159,9 @@ bool verify_result(fType* h_reference[], fType* h_result[], int size_per_stream,
     fType adaptive_tol = TOL * (1 + num_arccos_calls); 
     for (int i = 0; i < num_streams; ++i) {
         for (int j = 0; j < size_per_stream; ++j) {
-            if (std::fabs(h_reference[i][j] - h_result[i][j]) > TOL) {
+            if (std::fabs(h_reference[i][j] - h_result[i][j]) > adaptive_tol) {
                 std::cerr << "Mismatch at index " << j << " in stream " << i << ": "
-                          << h_reference[i][j] << " != " << h_result[i][j] << std::endl;
+                          << h_reference[i][j] << " != " << h_result[i][j] << " with a difference of " << std::fabs(h_reference[i][j] - h_result[i][j]) << std::endl;
                 return false; // Early exit on first mismatch
             }
         }
