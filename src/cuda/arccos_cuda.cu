b9d3c810606e45b8abe395d5415c673fa62c823c
Raphael Graf <raphael.graf01@gmail.com>
2025-07-09 14:27:45 +0200

    Add new measurments with multiple arccos calls

--- src/cuda/arccos_cuda.cu
+++ src/cuda/arccos_cuda.cu
@@ -156,10 +156,10 @@ cudaError_t run_stream_operations(fType* h_data[], fType* h_result[], fType* d_d
 
 // Verify the result of the arccos computation (return bool?) (call init_ref_result for the reference result)
 bool verify_result(fType* h_reference[], fType* h_result[], int size_per_stream, int num_streams, int num_arccos_calls) {
-    fType adaptive_tol = TOL * (1 + num_arccos_calls); 
+    // fType adaptive_tol = TOL * (1 + num_arccos_calls); 
     for (int i = 0; i < num_streams; ++i) {
         for (int j = 0; j < size_per_stream; ++j) {
-            if (std::fabs(h_reference[i][j] - h_result[i][j]) > adaptive_tol) {
+            if (std::fabs(h_reference[i][j] - h_result[i][j]) > 1e-4) {
                 std::cerr << "Mismatch at index " << j << " in stream " << i << ": "
                           << h_reference[i][j] << " != " << h_result[i][j] << " with a difference of " << std::fabs(h_reference[i][j] - h_result[i][j]) << std::endl;
                 return false; // Early exit on first mismatch
