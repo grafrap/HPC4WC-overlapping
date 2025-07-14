0cc2f256d0dcfd5e6346c3e755e6e31ab42ffbe8
Raphael Graf <raphael.graf01@gmail.com>
2025-07-14 15:37:44 +0200

    measurements different nz

--- run-stencil_scaling.sh
+++ run-stencil_scaling.sh
@@ -27,22 +27,22 @@ ERROR_FILE="../measurements/stencil_gpu_scaling_$SLURM_JOB_ID.err"
 mkdir -p ../measurements
 
 # Print header only once
-echo "ranks nx ny nz num_iter time num_streams" > $OUTPUT_FILE
+echo "ranks, nx, ny, nz, num_iter, time, num_streams" > $OUTPUT_FILE
 
 # Four-dimensional loop for comprehensive testing
 # Loop 1: Number of streams (2^0 to 2^9 = 1 to 512)
-for ((stream_exp=0; stream_exp<=8; stream_exp++)); do
-    streams=$((2**stream_exp))
+for z_size in 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536; do
     
     # Loop 2: Problem sizes (xy dimensions, powers of 2 from 2^3 to 2^15)
-    for ((xy_exp=3; xy_exp<=12; xy_exp++)); do
+    for ((xy_exp=7; xy_exp<=7; xy_exp++)); do
         XY_SIZE=$((2**xy_exp))
         
         # Loop 3: Z dimension (8 to 512, exponential steps)
-        for z_size in 64; do
             # Loop 4: Iteration counts (2^0 to 2^15 = 1 to 32768)
             for ((iter_exp=5; iter_exp<=10; iter_exp++)); do
                 iter=$((2**iter_exp))
+                for ((stream_exp=0; stream_exp<=8; stream_exp++)); do
+                    streams=$((2**stream_exp))
                 
                 # Run the test and append results
                 ./stencil2d_gpu_streams -nx $XY_SIZE -ny $XY_SIZE -nz $z_size -iter $iter -streams $streams >> $OUTPUT_FILE 2>> $ERROR_FILE
