1fdfbe2da19fb98028fbac3d1522bc2f58d5d4b5
Florian Stuehlinger <fstuehlinger@student.ethz.ch>
2025-07-16 12:12:17 +0200

    Replaced tolerance with the respective macro

--- src/cuda/arccos_cuda.cu
+++ src/cuda/arccos_cuda.cu
@@ -158,7 +158,7 @@ int verify_result(fType* h_reference[], fType* h_result[], int size_per_stream, 
     // Check if the results match the reference values
     for (int i = 0; i < num_streams; ++i) {
         for (int j = 0; j < size_per_stream; ++j) {
-            if (std::fabs(h_reference[i][j] - h_result[i][j]) > 1e-3) {
+            if (std::fabs(h_reference[i][j] - h_result[i][j]) > TOL) {
                 std::cerr << "Mismatch at index " << j << " in stream " << i << ": "
                           << h_reference[i][j] << " != " << h_result[i][j] << " with a difference of " << std::fabs(h_reference[i][j] - h_result[i][j]) << std::endl;
                 // Early exit on first mismatch
