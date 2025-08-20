12284e210d90ee7eb7b8a5bf4dde6a3d04c431de
starkp <starkp@student.ethz.ch>
2025-08-20 18:19:14 +0200

    choose smaller test array

--- src/gt4py/test_arccos_gt4py.py
+++ src/gt4py/test_arccos_gt4py.py
@@ -3,21 +3,24 @@ from arccos_gt4py import test_arccos
 import os
 import numpy as np
 
+###
+# Test arccos correctness for all provided numbers of arccos calls
+###
+
 print(f"{sys.argv[0]} started\n")
 
 sys.setrecursionlimit(5000)
 
-# ncalls and sizes as in run-arccos_cuda.sh
-ncalls = 2**np.arange(10)
-sizes = 2**np.arange(3,30,2)
-
-smax = sizes.max()
+ncalls = 2**np.arange(10)  # as in run-arccos_cuda.sh
+size = 8000  # small test array
 
 all_close = True
 for n in ncalls:
     try:
-        test_arccos(n, smax)
+        test_arccos(n, size)
+
     except Exception as e:
+        print(e)
         all_close = False
 
 if all_close:
